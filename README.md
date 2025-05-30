f5dac9f736d0fb460b18ee8be56301075d4c3cf8
saiharsha-k <saiharsha.msc@gmail.com>
2025-05-30 15:40:21 +0530

    Modified app.py and Readme.md

---
+++ README.md
@@ -0,0 +1,1 @@
+#This is a repository for Hyd-Batch 001.
--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+welcome_message = ''' Welcome to the Python application!
+This application is designed to demonstrate a simple Python script.
+'''
+
+
+print(welcome_message)
 
